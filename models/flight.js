967236fc43cc0d392b44369d6b5af913da87a7d6
lilliesheely <lilliesheely@gmail.com>
2022-07-14 16:17:51 -0700

    first commit

---
+++ models/flight.js
@@ -0,0 +1,16 @@
+
+
+const Flight = {
+    airline: String, 
+    airport: {
+        type: String, 
+        default: 'DEN'
+    }, 
+    flightNo: Number, 
+    departs: function() {
+        return 
+    }
+    Date
+}, { 
+    timestamps: true
+}
