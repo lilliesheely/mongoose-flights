f1be83d76272f5a9dba5756152148515bace8d9c
lilliesheely <lilliesheely@gmail.com>
2022-07-15 16:12:42 -0700

    Bonus #2 complete

--- controllers/flights.js
+++ controllers/flights.js
@@ -8,11 +8,19 @@ module.exports = {
 
 function index (req, res) {
     Flight.find({}, function(err, flights){
+        flights.sort((a,b) => Number(a.departs) - Number(b.departs));
         res.render('flights/index', {flights}); 
     });
 }
 
 function newFlight(req, res) {
+    const newFlight = new Flight();
+    const dt = newFlight.departs;
+
+    let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1).toString().padStart(2, '0')}`;
+    departsDate += `-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`;
+        res.render('flights/new', { departsDate });
+
     res.render('flights/new')
 }
 
