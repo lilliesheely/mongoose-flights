06119b524db6e49a669127b52a0817a780f93186
lilliesheely <lilliesheely@gmail.com>
2022-07-15 13:47:23 -0700

    Part 1 complete, w/o bonuses - default date fixed

--- models/flight.js
+++ models/flight.js
@@ -6,17 +6,26 @@ const flightSchema = new Schema ({
     airline: String, 
     airport: {
         type: String, 
-        default: 'DEN'
+        default: 'DEN' 
     }, 
     flightNo: {
         type: Number,
         min: 10, 
         max: 9999
     }, 
-    departs: Date 
+    departs: {
+        type: Date, 
+        default: function() {
+            let date = new Date(); 
+            return date.setFullYear(date.getFullYear() + 1);  
+        }
+    }
+}, {
+    timestamps: true
 });
 
 
 
 
 module.exports = mongoose.model('Flight', flightSchema);
+
