0b81df0f2b4e20de7a1034d5002624e668974e3a
lilliesheely <lilliesheely@gmail.com>
2022-07-21 09:56:25 -0700

    Updated css

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -2,25 +2,21 @@ const Ticket = require('../models/ticket');
 const Flight = require('../models/flight');
 
 module.exports = {
-    create,
-    new: newTicket
+    new: newTicket,
+    create
 };
 
+function newTicket(req, res){
+    res.render(`tickets/new`, { title: 'Add Ticket', flightId: req.params.id });
+}
+     
 function create(req, res){
     req.body.flight = req.params.id;
     Ticket.create(req.body, function(err, ticket) {
         res.redirect( `/flights/${req.params.id}`);
     })
 }
-
-
-function newTicket(req, res){
-    res.render(`tickets/new`, { title: 'Add Ticket', flightId: req.params.id });
-}
-
-
-      
-        
+   
 
 
 
